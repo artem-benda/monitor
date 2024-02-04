8f81c91ad02e9b6e1d59194f4322b0dae93ce9f3
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-02-04 09:55:55 +0300

    iter2 server handler tests
    refactor
    - Добавлены тесты для handler'a серверной части. Так же изменен способ создания handler'a для инъекции хранилища

--- internal/server/updateHandler.go
+++ internal/server/updateHandler.go
@@ -10,36 +10,39 @@ import (
 	"github.com/artem-benda/monitor/internal/storage"
 )
 
-func UpdateHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("UpdateHandler, method = %s, path = %s", r.Method, r.URL.Path)
-	switch params := strings.Split(r.URL.Path, "/"); {
-	case r.Method != http.MethodPost:
-		{
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	case len(params) > 0 && !model.ValidMetricKind(params[0]):
-		{
-			http.Error(w, "Metric type not supported", http.StatusBadRequest)
-		}
-	case len(params) < 2 || params[1] == "":
-		{
-			http.Error(w, "Metric name cannot be empty", http.StatusNotFound)
-		}
-	case len(params) < 3 || params[2] == "":
-		{
-			http.Error(w, "Invalid parameters values", http.StatusUnprocessableEntity)
-		}
-	case len(params) == 3 && model.ValidMetricKind(params[0]) && params[1] != "":
-		{
-			if err := service.UpdateMetric(storage.Store, params[0], params[1], params[2]); err == nil {
-				w.WriteHeader(http.StatusOK)
-			} else {
-				http.Error(w, "Bad metric value", http.StatusBadRequest)
+func MakeUpdateHandler(store storage.Storage) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("UpdateHandler, method = %s, path = %s", r.Method, r.URL.Path)
+		w.Header().Add("Content-type", "text/plain")
+		switch params := strings.Split(strings.TrimLeft(r.URL.Path, "/"), "/"); {
+		case r.Method != http.MethodPost:
+			{
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			}
-		}
-	default:
-		{
-			http.Error(w, "Method unimplemented", http.StatusNotImplemented)
+		case len(params) > 0 && !model.ValidMetricKind(params[0]):
+			{
+				http.Error(w, "Metric type not supported", http.StatusBadRequest)
+			}
+		case len(params) < 2 || params[1] == "":
+			{
+				http.Error(w, "Metric name cannot be empty", http.StatusNotFound)
+			}
+		case len(params) < 3 || params[2] == "":
+			{
+				http.Error(w, "Invalid parameters values", http.StatusUnprocessableEntity)
+			}
+		case len(params) == 3 && model.ValidMetricKind(params[0]) && params[1] != "":
+			{
+				if err := service.UpdateMetric(storage.Store, params[0], params[1], params[2]); err == nil {
+					w.WriteHeader(http.StatusOK)
+				} else {
+					http.Error(w, "Bad metric value", http.StatusBadRequest)
+				}
+			}
+		default:
+			{
+				http.Error(w, "Method unimplemented", http.StatusNotImplemented)
+			}
 		}
 	}
 }

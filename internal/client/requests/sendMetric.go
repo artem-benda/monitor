656906dc4439fb9befc224f8cf8a211eaa49e1e2
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-02-15 18:37:46 +0300

    refactor
    - перемещены файлы с функциями запросов клиента (агента)
    - в хранилище метрик и counter добавлен RWMutex

--- internal/client/requests/sendMetric.go
+++ internal/client/requests/sendMetric.go
@@ -1,4 +1,4 @@
-package client
+package requests
 
 import (
 	"fmt"
--- internal/client/storage/memCounter.go
+++ internal/client/storage/memCounter.go
@@ -1,23 +1,32 @@
 package storage
 
+import "sync"
+
 type memCounter struct {
 	currentValue uint64
+	rw           *sync.RWMutex
 }
 
 func NewCounter() Counter {
-	return &memCounter{}
+	return &memCounter{0, &sync.RWMutex{}}
 }
 
 func (c *memCounter) IncrementAndGet() uint64 {
+	c.rw.Lock()
+	defer c.rw.Unlock()
 	c.currentValue++
 	return c.currentValue
 }
 
 func (c memCounter) Get() uint64 {
+	c.rw.RLock()
+	defer c.rw.RUnlock()
 	return c.currentValue
 }
 
 func (c *memCounter) Reset() {
+	c.rw.Lock()
+	defer c.rw.Unlock()
 	c.currentValue = 0
 }
 

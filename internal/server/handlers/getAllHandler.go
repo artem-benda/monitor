19c53b747f2ec58d5e346efa0094d74317341cae
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-03-02 13:38:46 +0300

    Update getAllHandler.go
    fix test home page

--- internal/server/handlers/getAllHandler.go
+++ internal/server/handlers/getAllHandler.go
@@ -11,6 +11,7 @@ import (
 func MakeGetAllHandler(store storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-type", "text/html")
+		w.WriteHeader(http.StatusOK)
 		for metricKey, strVal := range service.GetAllMetrics(store) {
 			w.Write([]byte("<p>"))
 			w.Write([]byte(fmt.Sprintf("%s: %s", metricKey.Name, strVal)))

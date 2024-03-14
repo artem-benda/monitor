996f4df4601236b4ed49f15e4868769e004fc2d5
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-03-14 11:15:38 +0300

    Update sendAllMetrics.go
    refactor
    - agent to use new method "updates"

--- internal/client/requests/sendAllMetrics.go
+++ internal/client/requests/sendAllMetrics.go
@@ -1,20 +1,51 @@
 package requests
 
 import (
-	"log"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"net/http"
 
 	"github.com/artem-benda/monitor/internal/dto"
 	"github.com/artem-benda/monitor/internal/model"
 	"github.com/go-resty/resty/v2"
+	"github.com/mailru/easyjson"
 )
 
-func SendAllMetrics(resty *resty.Client, metrics map[model.MetricKey]model.MetricValue) {
-	for metric, val := range metrics {
-		dto := dto.Metrics{ID: metric.Name, MType: metric.Kind, Value: &val.Gauge, Delta: &val.Counter}
-		err := sendMetric(resty, dto)
-		if err != nil {
-			log.Printf("error sending metric %s/%s [ %f  %d ], %s", metric.Kind, metric.Name, val.Gauge, val.Counter, err)
-		}
+func SendAllMetrics(resty *resty.Client, metrics map[model.MetricKey]model.MetricValue) error {
+	dtos := make(dto.MetricsBatch, len(metrics))
+
+	for k, v := range metrics {
+		dtos = append(dtos, model.AsDto(k, v))
 	}
+
+	var (
+		json []byte
+		err  error
+	)
+
+	if json, err = easyjson.Marshal(dtos); err != nil {
+		return err
+	}
+
+	var b bytes.Buffer
+	w := gzip.NewWriter(&b)
+	if _, err := w.Write(json); err != nil {
+		return err
+	}
+	w.Close()
+
+	resp, err := resty.R().
+		SetBody(b.Bytes()).
+		SetHeader("Content-Encoding", "gzip").
+		SetHeader("Content-Type", "application/json").
+		Post("/updates/")
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return fmt.Errorf("http error, status code = %d", resp.StatusCode())
+	}
+	return nil
 
 }

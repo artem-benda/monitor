026ed5e79c43f5e495063e23fb74e519bb05e1b0
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-03-31 17:21:47 +0300

    agent's max parallel workers flags

---
+++ internal/client/service/readPSUtilsMetrics.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"github.com/artem-benda/monitor/internal/model"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+var (
+	totalMemoryMetric     = model.NewGaugeMetricKey("TotalMemory")
+	freeMemoryMetric      = model.NewGaugeMetricKey("FreeMemory")
+	cpuUtilization1Metric = model.NewGaugeMetricKey("CPUutilization1")
+)
+
+func readPSUtilsMetrics() map[model.MetricKey]model.MetricValue {
+	m := make(map[model.MetricKey]model.MetricValue)
+
+	v, _ := mem.VirtualMemory()
+	c, _ := cpu.Percent(0, false)
+
+	m[totalMemoryMetric] = model.MetricValue{Gauge: float64(v.Total)}
+	m[freeMemoryMetric] = model.MetricValue{Gauge: float64(v.Free)}
+	m[cpuUtilization1Metric] = model.MetricValue{Gauge: float64(c[0])}
+
+	return m
+}
